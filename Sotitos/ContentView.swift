80f82c3b9e727486ae12ff14e75e4ec20f5e5bb4
Daniel Lopez <lopezrivasdaniel@gmail.com>
2024-02-12 23:05:51 -0300

    first commit

---
+++ Sotitos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sotitos
+//
+//  Created by Daniel López Rivas on 12-02-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sotitos/SotitosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SotitosApp.swift
+//  Sotitos
+//
+//  Created by Daniel López Rivas on 12-02-24.
+//
+
+import SwiftUI
+
+@main
+struct SotitosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
